0a133ccab72f84c0384fab85d8bfd904ab66418a
Nick Joyce <nick.joyce@realkinetic.com>
2012-12-11 21:42:11 -0600

    Fix the imports

--- geventwebsocket/__init__.py
+++ geventwebsocket/__init__.py
@@ -3,5 +3,5 @@ __version__ = ".".join(map(str, version_info))
 
 __all__ = ['WebSocketHandler', 'WebSocketError']
 
-from geventwebsocket.handler import WebSocketHandler
-from geventwebsocket.websocket import WebSocketError
+from .handler import WebSocketHandler
+from .exceptions import WebSocketError
