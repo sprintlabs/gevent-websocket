558e8a5d98950a5245e2e3ccecc573170376894b
Nick Joyce <nick.joyce@realkinetic.com>
2012-12-31 00:25:31 +0000

    Setup metadata

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="gevent-websocket",
-    version="0.3.6",
+    version="0.4.0",
     description="Websocket handler for the gevent pywsgi server, a Python network library",
     long_description=open("README.rst").read(),
-    author="Jeffrey Gelens",
-    author_email="jeffrey@noppo.pro",
+    author="Nick Joyce",
+    author_email="nick.joyce@realkinetic.com",
     license="BSD",
-    url="https://bitbucket.org/Jeffrey/gevent-websocket",
-    download_url="https://bitbucket.org/Jeffrey/gevent-websocket",
-    install_requires=("gevent", "greenlet"),
+    url="https://github.com/njoyce/gevent-websocket",
+    download_url="https://github.com/njoyce/gevent-websocket",
+    install_requires=("gevent",),
     tests_require=(
         'nose',
         'autobahntestsuite',
         'mock'
     ),
-    packages=find_packages(exclude=["examples","tests"]),
+    packages=find_packages(exclude=["examples", "tests"]),
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
