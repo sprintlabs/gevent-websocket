eb2b54b108b84ad47f87912365367f8e4602c0fc
Nick Joyce <nick.joyce@realkinetic.com>
2013-01-02 21:35:07 +0000

    Add support for running the autobahn test suite in a scripted fashion.

---
+++ tests/run_autobahn_tests.py
@@ -0,0 +1,109 @@
+import sys
+
+import gevent
+from gevent import monkey, pywsgi, subprocess
+
+monkey.patch_all()
+
+import geventwebsocket
+
+
+def autobahn_echo(environ, start_response):
+    websocket = environ.get("wsgi.websocket")
+
+    if not websocket:
+        start_response('404 Not Found', [])
+
+        return []
+
+    try:
+        while True:
+            message = websocket.receive()
+
+            if message is None:
+                break
+
+            websocket.send(message)
+    except geventwebsocket.WebSocketError:
+        pass
+    finally:
+        websocket.close()
+
+
+def run_echo_server(address):
+    """
+    Run an echo websocket server compatible with the fuzzingclient of autobahn.
+
+    :param address: The host/port tuple where to host the echo server.
+    """
+    server = pywsgi.WSGIServer(
+        address, autobahn_echo,
+        handler_class=geventwebsocket.WebSocketHandler)
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+
+def run_autobahn_test_suite():
+    """
+    Spawn the autobahn test suite in a subprocess
+    """
+    cmd = ['wstest -m fuzzingclient -s autobahn.json']
+
+    wstest = subprocess.Popen(
+        cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+
+    if wstest.wait():
+        # something went wrong, it's boom time.
+        print wstest.stderr.read()
+        print wstest.stdout.read()
+
+        raise RuntimeError
+
+
+def waitany(events, timeout=None):
+    from gevent.event import AsyncResult
+
+    result = AsyncResult()
+    update = result.set
+
+    try:
+        for event in events:
+            if not event.started:
+                event.start()
+
+            if event.ready():
+                return event
+            else:
+                event.rawlink(update)
+
+        return result.get(timeout=timeout)
+    finally:
+        for event in events:
+            event.unlink(update)
+
+
+if __name__ == '__main__':
+    address = ('localhost', 8000)
+
+    echo_thread = gevent.spawn(run_echo_server, address)
+    wstest_thread = gevent.spawn(run_autobahn_test_suite)
+
+    ret = waitany([echo_thread, wstest_thread])
+
+    if ret == wstest_thread:
+        # the wstest thread ended first, let's just make sure it was successful
+        echo_thread.kill()
+
+        if not wstest_thread.successful():
+            raise SystemExit(1)
+
+        sys.exit(0)
+
+    # if we get here then the echo_thread ended first, which is not supposed to
+    # happen so die horribly.
+    wstest_thread.kill()
+
+    sys.exit(1)
