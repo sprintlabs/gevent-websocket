02ccaea63824694fd3781bd5e847e793245080fe
Nick Joyce <nick.joyce@realkinetic.com>
2013-01-03 19:30:17 +0000

    Make setup more pip/venv friendly.

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name="gevent-websocket",
     version="0.4.0",
-    description="Websocket handler for the gevent pywsgi server, a Python network library",
+    description=(
+        "Websocket handler for the gevent pywsgi server, "
+        "a Python network library"),
     long_description=open("README.rst").read(),
     author="Nick Joyce",
     author_email="nick.joyce@realkinetic.com",
     license="BSD",
     url="https://github.com/njoyce/gevent-websocket",
     download_url="https://github.com/njoyce/gevent-websocket",
-    install_requires=("gevent",),
-    tests_require=(
-        'nose',
-        'autobahntestsuite',
-        'mock'
-    ),
+    install_requires=open('requirements.txt').readlines(),
+    tests_require=open('requirements_dev.txt').readlines(),
+    setup_requires=['nose>=1.0'],
+    test_suite='nose.collector',
     packages=find_packages(exclude=["examples", "tests"]),
     classifiers=[
         "Development Status :: 4 - Beta",
