3599ab5013ceb50a3d68984b66225a495fad1ae3
Nick Joyce <nick.joyce@realkinetic.com>
2013-03-26 19:12:15 +0000

    Add `upgrade_websocket` to upmost api.

--- geventwebsocket/__init__.py
+++ geventwebsocket/__init__.py
@@ -1,7 +1,8 @@
 version_info = (0, 3, 6)
 __version__ = ".".join(map(str, version_info))
 
-__all__ = ['WebSocketHandler', 'WebSocketError']
+__all__ = ['WebSocketHandler', 'WebSocketError', 'upgrade_websocket']
 
 from .handler import WebSocketHandler
 from .exceptions import WebSocketError
+from .wsgi import upgrade_websocket
